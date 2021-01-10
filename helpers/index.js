36e1dae943244e61c551937928e962995fdf6b9d
realtg4 <realtg4@gmail.com>
2021-01-10 18:49:06 +0530

    watch time 20% to 90% random

--- helpers/index.js
+++ helpers/index.js
@@ -7,12 +7,24 @@ const watchVideosInSequence = async (page, ipAddr, targetUrlsList, durationInSec
   for (const url of targetUrlsList) {
     await page.goto(url, { waitUntil: 'load' });
     try {
-      await page.waitForSelector('.view-count', { timeout: 5000 });
+      await page.waitForSelector('.view-count', { timeout: 30000 });
+      await page.waitFor(30 * 1000);
+      await page.waitForSelector('.ytp-time-duration', { timeout: 30000 });
       await page.mouse.click(100, 100);
-      const duration = (durationInSeconds + _random(-(durationInSeconds / 6), (durationInSeconds / 6), true));
+      const totalTime = await page.evaluate(() => {
+        return Array.from(document.getElementsByClassName("ytp-time-duration")).map(d => {
+          const min = parseInt(d.innerText.split(":")[0]) * 60
+          return parseInt(d.innerText.split(":")[1]) + min 
+        });
+      });
+      const percentageToWatch = 30 + Math.ceil(Math.random()*100) % 70;
+      const duration = Math.ceil((totalTime/100) * percentageToWatch);
+      // console.log(totalTime,percentageToWatch,duration)
       await page.waitFor(duration * 1000);
+      await page.mouse.click(100, 100);
       await logger.logCount(page, url, ipAddr, duration);
-    } catch {
+    } catch(e) {
+      console.error("Exception thrown", e.stack);
       logger.logFailedAttempt(url, ipAddr);
     }
   }
